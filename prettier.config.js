e0c813a759487517a884d20bc93e6c24bbe8fb81
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-26 19:01:33 +0545

    prettier.config.js: Only keep the prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -2,11 +2,7 @@
 
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  semi: false,
-  singleQuote: true,
-  trailingComma: "es5",
-  tabWidth: 4,
-};
+    plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
