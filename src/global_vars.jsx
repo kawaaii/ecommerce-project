bf6a71d1d1976784d5bc4a5ce69d7589bf9318bf
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 11:34:53 +0545

    Add placebo global_vars

---
+++ src/global_vars.jsx
@@ -0,0 +1,11 @@
+const globalVars = () => {
+  return (
+    <>
+      <div className="main">
+        placeholder this whole section need to be redone
+      </div>
+    </>
+  );
+};
+
+export default globalVars;
