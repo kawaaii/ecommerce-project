68a9e7b33adc22334e555f7ea74e9c62e45eec8e
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 15:38:24 +0545

    darkMode: Fix common rendering emulation

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,22 +1,43 @@
 import { useEffect, useState } from "react";
 
 export const useDarkMode = () => {
-  const [isDarkMode, setDarkMode] = useState(() => {
-    const storedTheme = localStorage.theme;
-    const prefersDarkMode = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
+  const isWindowDefined = typeof window !== "undefined";
+
+  const getInitialDarkMode = () => {
+    const storedTheme = isWindowDefined ? localStorage.theme : null;
+    const prefersDarkMode =
+      isWindowDefined &&
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
     return storedTheme === "dark" || (!storedTheme && prefersDarkMode);
-  });
+  };
+
+  const [isDarkMode, setDarkMode] = useState(getInitialDarkMode);
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", isDarkMode);
-    localStorage.theme = isDarkMode ? "dark" : "light";
-  }, [isDarkMode]);
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-  const toggleDarkMode = () => {
-    setDarkMode(!isDarkMode);
-  };
+    const handleChange = (e) => setDarkMode(e.matches);
+
+    if (mediaQuery.addEventListener)
+      mediaQuery.addEventListener("change", handleChange);
+    else mediaQuery.addListener(handleChange);
+
+    return () => {
+      if (mediaQuery.removeEventListener)
+        mediaQuery.removeEventListener("change", handleChange);
+      else mediaQuery.removeListener(handleChange);
+    };
+  }, [isWindowDefined]); // Include isWindowDefined in the dependency array
+
+  useEffect(() => {
+    if (isWindowDefined) {
+      document.documentElement.classList.toggle("dark", isDarkMode);
+      localStorage.theme = isDarkMode ? "dark" : "light";
+    }
+  }, [isDarkMode, isWindowDefined]); // Include isDarkMode and isWindowDefined in the dependency array
+
+  const toggleDarkMode = () => setDarkMode((prevDarkMode) => !prevDarkMode);
 
   return { isDarkMode, toggleDarkMode };
 };
