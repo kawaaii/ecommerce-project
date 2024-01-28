870d3357875866d5ea7f699826454c05347fc35f
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 14:12:46 +0545

    NavigationBar: Fix up darkMode toggle

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import "./NavigationBar.css";
 
 import MagnifyingGlassIcon from "../../images/icons/MagnifyingGlassIcon";
@@ -5,74 +6,63 @@ import UserCircleIcon from "../../images/icons/UserCircleIcon";
 import SunIcon from "../../images/icons/SunIcon";
 import MoonIcon from "../../images/icons/MoonIcon";
 
-function darkMode() {
-  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-  if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = "light";
-
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = "dark";
-
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem("theme");
-}
-
-function DarkModeIconToggle() {
-  if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    return <MoonIcon />;
-  } else {
-    return <SunIcon />;
-  }
-}
-
 const NavigationBar = () => {
+  const [isDarkMode, setDarkMode] = useState(() => {
+    const storedTheme = localStorage.theme;
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+
+    return storedTheme === "dark" || (!storedTheme && prefersDarkMode);
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDarkMode);
+    localStorage.theme = isDarkMode ? "dark" : "light";
+  }, [isDarkMode]);
+
+  const handleDarkModeToggle = () => {
+    setDarkMode(!isDarkMode);
+  };
+
   return (
-    <nav className="container flex flex-row justify-between">
-      <div className="navBarLogo">
-        <img src="#!" alt="LOGO" />
-      </div>
-      <div className="searchBar">
-        <form className="flex gap-1 align-middle" action="#!">
-          <div className="relative">
-            <input
-              type="text"
-              className="rounded text-black placeholder-black ring-1 ring-[#0b0c0f] dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
-              placeholder="WIP"
-            />
-            <button type="submit" className="absolute inset-y-0 right-0 pr-1 ">
-              <MagnifyingGlassIcon />
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="navBarItems flex flex-row gap-2">
-        <button className="hover:text-[#9499ff]">Login / Register</button>
-        <a href="#!" className="hover:text-[#9499ff]">
-          <UserCircleIcon />
-        </a>
-        <button
-          onClick={darkMode}
-          className="rounded-full"
-          aria-label="Toggle Dark Mode"
-        >
-          <DarkModeIconToggle />
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="container flex flex-row justify-between">
+        <div className="navBarLogo">
+          <img src="#!" alt="LOGO" />
+        </div>
+        <div className="searchBar">
+          <form className="flex gap-1 align-middle" action="#!">
+            <div className="relative">
+              <input
+                type="text"
+                className="rounded text-black placeholder-black ring-1 ring-[#0b0c0f] dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
+                placeholder="WIP"
+              />
+              <button
+                type="submit"
+                className="absolute inset-y-0 right-0 pr-1 "
+              >
+                <MagnifyingGlassIcon />
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="navBarItems flex flex-row gap-2">
+          <button className="hover:text-[#9499ff]">Login / Register</button>
+          <a href="#!" className="hover:text-[#9499ff]">
+            <UserCircleIcon />
+          </a>
+          <button
+            onClick={handleDarkModeToggle}
+            className="rounded-full"
+            aria-label="Toggle Dark Mode"
+          >
+            {isDarkMode ? <MoonIcon /> : <SunIcon />}
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
