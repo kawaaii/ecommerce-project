287425e0f6030fe2a11ed74a4628bf94c46fde67
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 15:10:19 +0545

    PageNotFound: Beautify 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,9 +2,10 @@ const PageNotFound = () => {
   return (
     <>
       <section className="container flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center">
-          <p>404</p>
-          <p>Page Not Found</p>
+        <div className="flex flex-col items-center gap-4 font-mono">
+          <p className="text-9xl font-black">404</p>
+          <p className="text-4xl">Page Not Found</p>
+          <p className="text-9xl">&#129335;</p>
         </div>
       </section>
     </>
