0cef43de4eb584aedcc7395b5555d6974392eb35
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 16:06:22 +0545

    NavigationBar: Misc improvements

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -10,7 +10,7 @@ const NavigationBar = () => {
 
   return (
     <>
-      <section className="container flex flex-row justify-between">
+      <section className="container flex justify-between">
         <div className="navBarLogo">
           <img src="#!" alt="LOGO" />
         </div>
@@ -22,20 +22,14 @@ const NavigationBar = () => {
                 className="rounded text-black placeholder-black ring-1 ring-[#0b0c0f] placeholder:text-center placeholder:decoration-transparent dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
                 placeholder="WIP"
               />
-              <button
-                type="submit"
-                className="absolute inset-y-0 right-0 pr-1 "
-              >
+              <button type="submit" className="absolute inset-y-0 right-0 pr-1">
                 <MagnifyingGlassIcon />
               </button>
             </div>
           </form>
         </div>
-        <div className="navBarItems flex flex-row gap-3">
-          <button
-            href="#!"
-            className="flex flex-row gap-2 hover:text-[#9499ff]"
-          >
+        <div className="navBarItems flex gap-3">
+          <button href="#!" className="flex gap-2 hover:text-[#9499ff]">
             <span>
               <UserCircleIcon />
             </span>
@@ -44,7 +38,7 @@ const NavigationBar = () => {
           <button
             onClick={toggleDarkMode}
             className="rounded-full"
-            aria-label="Toggle Dark Mode"
+            aria-label="Dark/Light Mode Toggle"
           >
             {isDarkMode ? <MoonIcon /> : <SunIcon />}
           </button>
