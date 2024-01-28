6af5e3fce4504d80e21b10269869880adf1a9a49
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-28 16:26:41 +0545

    NavigationBar: Misc CSS and SVG improvements

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,49 +2,48 @@ import MagnifyingGlassIcon from "../images/icons/MagnifyingGlassIcon";
 import UserCircleIcon from "../images/icons/UserCircleIcon";
 import SunIcon from "../images/icons/SunIcon";
 import MoonIcon from "../images/icons/MoonIcon";
-
+import ShopIcon from "../images/icons/ShopIcon";
 import { useDarkMode } from "./DarkMode";
 
 const NavigationBar = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <>
-      <section className="container flex justify-between">
-        <div className="navBarLogo">
-          <img src="#!" alt="LOGO" />
-        </div>
-        <div className="searchBar">
-          <form className="flex gap-1 align-middle" action="#!">
-            <div className="relative">
-              <input
-                type="text"
-                className="rounded text-black placeholder-black ring-1 ring-[#0b0c0f] placeholder:text-center placeholder:decoration-transparent dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
-                placeholder="WIP"
-              />
-              <button type="submit" className="absolute inset-y-0 right-0 pr-1">
-                <MagnifyingGlassIcon />
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="navBarItems flex gap-3">
-          <button href="#!" className="flex gap-2 hover:text-[#9499ff]">
-            <span>
-              <UserCircleIcon />
-            </span>
-            Login / Register
-          </button>
-          <button
-            onClick={toggleDarkMode}
-            className="rounded-full"
-            aria-label="Dark/Light Mode Toggle"
-          >
-            {isDarkMode ? <MoonIcon /> : <SunIcon />}
-          </button>
-        </div>
-      </section>
-    </>
+    <section className="container flex items-center justify-between p-5 backdrop-blur-md">
+      <div className="navBarLogo flex flex-row items-center gap-2">
+        <ShopIcon className="text-5xl text-black dark:text-white" />
+        <strong>Hamro Pasal</strong>
+      </div>
+      <div className="searchBar">
+        <form className="flex gap-1 align-middle" action="#!">
+          <div className="relative">
+            <input
+              type="text"
+              className="h-9 w-72 rounded-full text-center text-black placeholder-black ring-1 ring-[#0b0c0f] placeholder:text-center placeholder:decoration-transparent dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
+              placeholder="WIP"
+            />
+            <button type="submit" className="absolute inset-y-0 right-2 pr-1">
+              <MagnifyingGlassIcon className=" ml-[-0.5rem]" />
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="navBarItems flex gap-3">
+        <button href="#!" className="flex gap-2 hover:text-[#9499ff]">
+          <span>
+            <UserCircleIcon />
+          </span>
+          Login / Register
+        </button>
+        <button
+          onClick={toggleDarkMode}
+          className="rounded-full"
+          aria-label="Dark/Light Mode Toggle"
+        >
+          {isDarkMode ? <MoonIcon /> : <SunIcon />}
+        </button>
+      </div>
+    </section>
   );
 };
 
