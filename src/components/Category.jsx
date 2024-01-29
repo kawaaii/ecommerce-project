759f66e5841854c12edcace2a817ea55e40b3d98
Hridaya <info.hridayaprajapati@gmail.com>
2024-01-29 18:37:03 +0545

    Category: Adapt with recent json changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -20,8 +20,9 @@ const Category = () => {
   const groupedProducts = useMemo(() => {
     return products.reduce((groupedProductsObj, product) => {
       const { category } = product;
-      groupedProductsObj[category] = [
-        ...(groupedProductsObj[category] || []),
+      const categoryName = category.name;
+      groupedProductsObj[categoryName] = [
+        ...(groupedProductsObj[categoryName] || []),
         product,
       ];
       return groupedProductsObj;
@@ -34,57 +35,61 @@ const Category = () => {
         <p>Loading...</p>
       ) : (
         <section className="container mt-10 flex flex-col gap-14">
-          {Object.keys(groupedProducts).map((category) => (
-            <div className="flex flex-col gap-4" key={category}>
-              <div className="mb-3 flex items-center justify-between">
-                <h2 className="text-2xl font-bold">{category}</h2>
-                <a
-                  href={`/products/${category}`}
-                  className="text-blue-500 hover:underline focus:outline-none"
-                >
-                  Shop More
-                </a>
-              </div>
-              <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {groupedProducts[category]
-                  .slice(0, 4)
-                  .map(({ id, title, image, price, rating }) => (
-                    <div
-                      key={id}
-                      className="product-card flex flex-col gap-4 rounded-lg border border-gray-300 dark:border-none dark:bg-gray-800"
-                    >
-                      <img
-                        src={image}
-                        className="mx-auto h-60 w-full rounded-t-md bg-white object-contain p-5"
-                        alt={title}
-                      />
-                      <div className="flex flex-col items-center">
-                        <h3 className="text-md mb-2 text-center font-semibold">
-                          {title}
-                        </h3>
-                        <p className="font-mono font-bold text-green-600">
-                          ${price.toLocaleString()}
-                        </p>
-                        <div className="flex items-center">
-                          <span className="text-yellow-500">
-                            ★ {rating.rate}
-                          </span>
-                          <span className="ml-1 text-gray-500">
-                            ({rating.count} reviews)
-                          </span>
+          {Object.keys(groupedProducts).map((categoryName) => {
+            const categorySection =
+              groupedProducts[categoryName][0].category.section;
+            return (
+              <div className="flex flex-col gap-4" key={categoryName}>
+                <div className="mb-3 flex items-center justify-between">
+                  <h2 className="text-2xl font-bold">{categoryName}</h2>
+                  <a
+                    href={`/products/${categorySection}`}
+                    className="text-blue-500 hover:underline focus:outline-none"
+                  >
+                    Shop More
+                  </a>
+                </div>
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                  {groupedProducts[categoryName]
+                    .slice(0, 4)
+                    .map(({ id, title, image, price, rating }) => (
+                      <div
+                        key={id}
+                        className="product-card flex flex-col gap-4 rounded-lg border border-gray-300 dark:border-none dark:bg-gray-800"
+                      >
+                        <img
+                          src={image}
+                          className="mx-auto h-60 w-full rounded-t-md bg-white object-contain p-5"
+                          alt={title}
+                        />
+                        <div className="flex flex-col items-center">
+                          <h3 className="text-md mb-2 text-center font-semibold">
+                            {title}
+                          </h3>
+                          <p className="font-mono font-bold text-green-600">
+                            ${price.toLocaleString()}
+                          </p>
+                          <div className="flex items-center">
+                            <span className="text-yellow-500">
+                              ★ {rating.rate}
+                            </span>
+                            <span className="ml-1 text-gray-500">
+                              ({rating.count} reviews)
+                            </span>
+                          </div>
+                          <button
+                            className="focus:shadow-outline-blue mb-5 mt-8 rounded-full bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600 focus:outline-none"
+                            onClick={() => alert(`Added ${title} to cart`)}
+                          >
+                            Add to Cart
+                          </button>
                         </div>
-                        <button
-                          className="focus:shadow-outline-blue mb-5 mt-8 rounded-full bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600 focus:outline-none"
-                          onClick={() => alert(`Added ${title} to cart`)}
-                        >
-                          Add to Cart
-                        </button>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </section>
       )}
     </>
