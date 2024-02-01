29756ada33c6a9da159404e6a8b61111bafd8e76
Hridaya <info.hridayaprajapati@gmail.com>
2024-02-01 20:27:56 +0545

    Fixup WorkInProgress component

--- src/pages/WorkInProgress.jsx
+++ src/pages/WorkInProgress.jsx
@@ -4,12 +4,8 @@ const WorkInProgress = () => {
       {" "}
       <section className="container flex flex-col items-center justify-center">
         <div className="flex flex-col items-center gap-4 font-mono">
-          <img
-            src="https://img.freepik.com/free-photo/worker-with-screwdrive_1401-3.jpg?w=740&t=st=1706798109~exp=1706798709~hmac=06d10ade14c6c290ad5f907b062c24bc4c5e5a2f3ad97a6cca54abc90a340fac"
-            alt=""
-          />
-          <p className="text-4xl">Work In Progress</p>
-          <p className="text-9xl">&#129335;</p>
+          <img src="WorkInProgress.png" alt="" />
+          <p className="text-4xl">This page is under construction.</p>
         </div>
       </section>
     </>
