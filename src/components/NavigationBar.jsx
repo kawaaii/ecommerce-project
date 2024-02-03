310de06761f160470ad8c230cd6824662474b98c
Hridaya <info.hridayaprajapati@gmail.com>
2024-02-03 17:18:43 +0545

    NavigationBar: Fix button
    Error: Buttons must have discernible text: Element has no title attribute

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -30,14 +30,21 @@ const NavigationBar = () => {
                 className="h-9 w-72 rounded-full text-center text-black placeholder-black ring-1 ring-[#0b0c0f] placeholder:text-center placeholder:decoration-transparent dark:bg-neutral-900 dark:text-white dark:placeholder-white dark:ring-1 dark:ring-[#d1d1d1]"
                 placeholder="Work In Progress"
               />
-              <button type="submit" className="absolute inset-y-0 right-2 pr-1">
+              <button
+                type="submit"
+                className="absolute inset-y-0 right-2 pr-1"
+                title="Search"
+              >
                 <MagnifyingGlassIcon className="ml-[-0.5rem]" />
               </button>
             </div>
           </form>
         </div>
         <div className="navBarItems flex items-center gap-3">
-          <button className="rounded-full p-2 hover:bg-slate-300 dark:hover:bg-neutral-700">
+          <button
+            className="rounded-full p-2 hover:bg-slate-300 dark:hover:bg-neutral-700"
+            title="View Shopping Cart"
+          >
             <CartIcon />
           </button>
           <a
@@ -53,6 +60,7 @@ const NavigationBar = () => {
             onClick={toggleDarkMode}
             className="rounded-full p-2 transition duration-300 ease-in-out hover:bg-slate-300 dark:hover:bg-neutral-700"
             aria-label="Dark/Light Mode Toggle"
+            title={`Switch to ${isDarkMode ? "Light" : "Dark"} Mode`}
           >
             {isDarkMode ? <MoonIcon /> : <SunIcon />}
           </button>
